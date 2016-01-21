523c9a0d198a4fff3ffdb2d18cc136912df3b9fc
Kevin Coffey <kevdougful@gmail.com>
2016-01-21 00:29:23 -0600

    added isActual call

--- server/migrations/update-schemas.js
+++ server/migrations/update-schemas.js
@@ -1,16 +1,14 @@
 var server = require('../server');
 var ds = server.dataSources.dev;
 
-ds.autoupdate();
-
-// ds.isActual(function(err, inSync) {
-//     if (!inSync) {
-//         ds.autoupdate(function(err, result) {
-//             if (err) {
-//                 throw err;
-//             } else {
-//                 console.log(result);
-//             }
-//         });
-//     }
-// });
+ds.isActual(function(err, inSync) {
+    if (!inSync) {
+        ds.autoupdate(function(err, result) {
+            if (err) {
+                throw err;
+            } else {
+                ds.disconnect();
+            }
+        });
+    }
+});
